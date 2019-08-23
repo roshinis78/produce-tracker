e6585c329cd573e2203448cd9855533e06cbb65d
Roshini Saravanakumar <roshini2@illinois.edu>
2019-08-23 11:41:49 -0500

    Added country names + import country dot

--- scripts/trade.js
+++ scripts/trade.js
@@ -16,6 +16,7 @@ var tradeJSON = null;
 var activeCountryIDs = new Map();
 var colorIndex = 0;
 const colorStep = 28;
+var whichGoods = "previous_goods_only";
 
 $(function() {
   // add event listeners
@@ -28,9 +29,22 @@ $(function() {
     }
   });
 
+  // show all goods or just produce
+  $("#all-goods-radio, #only-produce-radio").on("click", function() {
+    whichGoods = $(this).val();
+
+    // make a copy of the active countries from the map key iterator
+    var countriesToRedraw = [];
+    for(let country of activeCountryIDs.keys()){
+      countriesToRedraw.push(country);
+    }
+
+    // to be continued...
+  });
+
   // add selected country to display on button click if it is not already being displayed
   $("#add-country-button").on("click", function() {
-    if (!activeCountryIDs.has($("#add-country-select").val())) {
+    if (($("#add-country-select").val() != null) && (!activeCountryIDs.has($("#add-country-select").val()))) {
       // add the country to the display
       addedCountry = $("#add-country-select").val();
       activeCountryIDs.set(addedCountry, createCountryID(addedCountry));
@@ -47,11 +61,16 @@ $(function() {
           "<i class='fas fa-square mr-2' style='color:" +
             line.colors[colorIndex] +
             "'></i>" +
-            "<span>" +
             addedCountry +
-            "</span>" +
-            "<button type='button' class='remove-country-button' onclick='removeCountry(this)'><i class='fas fa-minus-circle'></i></button>"
+            "<button type='button' class='remove-country-button' value='" +
+            addedCountry +
+            "'><i class='fas fa-minus-circle'></i></button>"
         );
+
+      $(".remove-country-button:last").on("click", function() {
+        console.log("hello!")
+          removeCountry($(this).val());
+      });
 
       console.log("Added " + addedCountry + " to display!");
     }
@@ -126,7 +145,7 @@ function visualizeTrade() {
     .append("g")
     .attr("id", activeCountryIDs.get(addedCountry) + "-trade-line")
     .selectAll("path")
-    .data(tradeJSON[addedCountry]["previous_goods_only"])
+    .data(tradeJSON[addedCountry][whichGoods])
     .enter()
     .append("path")
     .datum(function(good) {
@@ -145,20 +164,81 @@ function visualizeTrade() {
     .style("stroke-width", "2px");
 
   // circles for countries
+  var toCountryCoordinates = null;
   tradeSVG
     .select(
       "#trade-lines #" + activeCountryIDs.get(addedCountry) + "-trade-line"
     )
     .selectAll("circle")
-    .data(tradeJSON[addedCountry]["previous_goods_only"])
+    .data(tradeJSON[addedCountry][whichGoods])
     .enter()
     .append("circle")
     .attr("r", line.pointRadius)
-    .attr("cx", function(good) {
+    .attr("cx", function(good, i) {
+      // record the coordinates of the country which is importing the goods once
+      if (i == 0) {
+        toCountryCoordinates = good["Coordinates"]["to_country"];
+      }
       return mapProjection(good["Coordinates"]["from_country"])[0];
     })
     .attr("cy", function(good) {
       return mapProjection(good["Coordinates"]["from_country"])[1];
+    });
+
+  // add a dot for the 'to country' -- the country which is importing the goods
+  tradeSVG
+    .select(
+      "#trade-lines #" + activeCountryIDs.get(addedCountry) + "-trade-line"
+    )
+    .append("circle")
+    .attr("r", line.pointRadius)
+    .attr("cx", mapProjection(toCountryCoordinates)[0])
+    .attr("cy", mapProjection(toCountryCoordinates)[1]);
+
+  // add country labels
+  d3.select("#trade-chart-labels")
+    .append("div")
+    .attr("id", activeCountryIDs.get(addedCountry) + "-labels")
+    .selectAll("div")
+    .data(function() {
+      // create a set containing all the 'from countries' and the 'to country'
+      var countrySet = new Set(addedCountry);
+      var countryArray = [];
+
+      tradeJSON[addedCountry][whichGoods].forEach(function(good, i) {
+        if (!countrySet.has(good["from_country"])) {
+          countrySet.add(good["from_country"]);
+
+          // push the first good twice so we can have data for the 'to country'
+          if (i == 0) {
+            countryArray.push(good);
+          }
+          countryArray.push(good);
+        }
+      });
+
+      return countryArray;
+    })
+    .enter()
+    .append("div")
+    .style("position", "absolute")
+    .style("top", function(good, i) {
+      if (i == 0) {
+        return mapProjection(good["Coordinates"]["to_country"])[1] + "px";
+      }
+      return mapProjection(good["Coordinates"]["from_country"])[1] + "px";
+    })
+    .style("left", function(good, i) {
+      if (i == 0) {
+        return mapProjection(good["Coordinates"]["to_country"])[0] + "px";
+      }
+      return mapProjection(good["Coordinates"]["from_country"])[0] + "px";
+    })
+    .html(function(good, i) {
+      if (i == 0) {
+        return good["to_country"];
+      }
+      return good["from_country"];
     });
 }
 
@@ -174,10 +254,7 @@ function createCountryID(country) {
 }
 
 // remove a country from the display and display list
-function removeCountry(buttonElement) {
-  var countryToDelete = $(buttonElement)
-    .prev()
-    .html();
+function removeCountry(countryToDelete) {
   var countryToDeleteID = activeCountryIDs.get(countryToDelete);
 
   // remove country from list
@@ -185,6 +262,7 @@ function removeCountry(buttonElement) {
 
   // remove country from the display
   $("#" + countryToDeleteID + "-trade-line").remove();
+  $("#" + countryToDeleteID + "-labels").remove();
   activeCountryIDs.delete(countryToDelete);
 
   console.log("Removed " + countryToDelete + " from display");
