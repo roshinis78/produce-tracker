02974a703da01a23b055c099cb641caf1c1f5a18
Roshini Saravanakumar <roshini2@illinois.edu>
2019-08-25 11:05:18 -0500

    Added show only produce vs show all goods functionality

--- scripts/trade.js
+++ scripts/trade.js
@@ -6,7 +6,11 @@ const margin = { top: 35, left: 120, bottom: 40, right: 10 };
 
 var line = {
   pointRadius: 2,
-  colors: null
+  colors: null,
+  label: {
+    letterWidth: 7,
+    verticalOffset: 13
+  }
 };
 
 var tradeSVG = null;
@@ -16,66 +20,9 @@ var tradeJSON = null;
 var activeCountryIDs = new Map();
 var colorIndex = 0;
 const colorStep = 28;
-var whichGoods = "previous_goods_only";
+var relevantTradeData = null;
 
 $(function() {
-  // add event listeners
-  // collapse the toolbar on click
-  $(".toolbar-title").on("click", function() {
-    if ($(".toolbar").css("left") == "0px") {
-      $(".toolbar").css("left", "-25%");
-    } else {
-      $(".toolbar").css("left", "0%");
-    }
-  });
-
-  // show all goods or just produce
-  $("#all-goods-radio, #only-produce-radio").on("click", function() {
-    whichGoods = $(this).val();
-
-    // make a copy of the active countries from the map key iterator
-    var countriesToRedraw = [];
-    for(let country of activeCountryIDs.keys()){
-      countriesToRedraw.push(country);
-    }
-
-    // to be continued...
-  });
-
-  // add selected country to display on button click if it is not already being displayed
-  $("#add-country-button").on("click", function() {
-    if (($("#add-country-select").val() != null) && (!activeCountryIDs.has($("#add-country-select").val()))) {
-      // add the country to the display
-      addedCountry = $("#add-country-select").val();
-      activeCountryIDs.set(addedCountry, createCountryID(addedCountry));
-      visualizeTrade();
-
-      // add the country to the display list
-      d3.select("#current-view-list")
-        .append("li")
-        .attr("width", "100%")
-        .attr("id", activeCountryIDs.get(addedCountry) + "-list-element")
-        .append("h5")
-        .attr("class", "d-flex")
-        .html(
-          "<i class='fas fa-square mr-2' style='color:" +
-            line.colors[colorIndex] +
-            "'></i>" +
-            addedCountry +
-            "<button type='button' class='remove-country-button' value='" +
-            addedCountry +
-            "'><i class='fas fa-minus-circle'></i></button>"
-        );
-
-      $(".remove-country-button:last").on("click", function() {
-        console.log("hello!")
-          removeCountry($(this).val());
-      });
-
-      console.log("Added " + addedCountry + " to display!");
-    }
-  });
-
   // read the map data
   d3.json("data/countries.json").then(function(data) {
     // create the SVG
@@ -109,11 +56,37 @@ $(function() {
           .html(function(country) {
             return country;
           });
+
+        // add event listeners
+        // add event listeners
+        // collapse the toolbar on click
+        $(".toolbar-title").on("click", function() {
+          if ($(".toolbar").css("left") == "0px") {
+            $(".toolbar").css("left", "-25%");
+          } else {
+            $(".toolbar").css("left", "0%");
+          }
+        });
+
+        // switch display to show all goods or just produce
+        $("#all-goods-radio, #only-produce-radio").on("click", function() {
+          if ($(this).val() == "only-produce") {
+            $(".all-goods").css("opacity", 0);
+            $(".only-produce").css("opacity", 1);
+          } else {
+            $(".all-goods").css("opacity", 1);
+            $(".only-produce").css("opacity", 0);
+          }
+        });
+
+        // add selected country to display on button click if it is not already being displayed
+        $("#add-country-button").on("click", addCountry);
       });
     });
   });
 });
 
+// draw the map to the display using a mercator projection
 function drawMap(data) {
   mapProjection = d3.geoMercator().fitWidth(width, data);
   var pathGenerator = d3.geoPath(mapProjection);
@@ -129,6 +102,7 @@ function drawMap(data) {
     .attr("fill", "white");
 }
 
+// visualize the import or export trade lines for the most recently added country
 function visualizeTrade() {
   // lines for trade paths
   var pathGenerator = d3
@@ -145,9 +119,19 @@ function visualizeTrade() {
     .append("g")
     .attr("id", activeCountryIDs.get(addedCountry) + "-trade-line")
     .selectAll("path")
-    .data(tradeJSON[addedCountry][whichGoods])
+    .data(function() {
+      return (relevantTradeData = tradeJSON[addedCountry][
+        "previous_goods_only"
+      ].concat(tradeJSON[addedCountry]["all_goods"]));
+    })
     .enter()
     .append("path")
+    .attr("class", function(good, i) {
+      if (i < tradeJSON[addedCountry]["previous_goods_only"].length) {
+        return "only-produce";
+      }
+      return "all-goods";
+    })
     .datum(function(good) {
       return [
         good["Coordinates"]["to_country"],
@@ -163,76 +147,51 @@ function visualizeTrade() {
     })
     .style("stroke-width", "2px");
 
-  // circles for countries
-  var toCountryCoordinates = null;
-  tradeSVG
-    .select(
-      "#trade-lines #" + activeCountryIDs.get(addedCountry) + "-trade-line"
-    )
-    .selectAll("circle")
-    .data(tradeJSON[addedCountry][whichGoods])
-    .enter()
-    .append("circle")
-    .attr("r", line.pointRadius)
-    .attr("cx", function(good, i) {
-      // record the coordinates of the country which is importing the goods once
-      if (i == 0) {
-        toCountryCoordinates = good["Coordinates"]["to_country"];
-      }
-      return mapProjection(good["Coordinates"]["from_country"])[0];
-    })
-    .attr("cy", function(good) {
-      return mapProjection(good["Coordinates"]["from_country"])[1];
-    });
-
-  // add a dot for the 'to country' -- the country which is importing the goods
-  tradeSVG
-    .select(
-      "#trade-lines #" + activeCountryIDs.get(addedCountry) + "-trade-line"
-    )
-    .append("circle")
-    .attr("r", line.pointRadius)
-    .attr("cx", mapProjection(toCountryCoordinates)[0])
-    .attr("cy", mapProjection(toCountryCoordinates)[1]);
-
   // add country labels
   d3.select("#trade-chart-labels")
     .append("div")
     .attr("id", activeCountryIDs.get(addedCountry) + "-labels")
     .selectAll("div")
-    .data(function() {
-      // create a set containing all the 'from countries' and the 'to country'
-      var countrySet = new Set(addedCountry);
-      var countryArray = [];
-
-      tradeJSON[addedCountry][whichGoods].forEach(function(good, i) {
-        if (!countrySet.has(good["from_country"])) {
-          countrySet.add(good["from_country"]);
-
-          // push the first good twice so we can have data for the 'to country'
-          if (i == 0) {
-            countryArray.push(good);
-          }
-          countryArray.push(good);
-        }
-      });
-
-      return countryArray;
-    })
+    .data([relevantTradeData[0], ...relevantTradeData])
     .enter()
     .append("div")
+    .attr("class", function(good, i){
+      if(i == 0){
+        return '';
+      }
+      else if (i <= (tradeJSON[addedCountry]["previous_goods_only"]).length) {
+        return "only-produce"
+      }
+      return "all-goods"
+    })
     .style("position", "absolute")
     .style("top", function(good, i) {
       if (i == 0) {
-        return mapProjection(good["Coordinates"]["to_country"])[1] + "px";
+        return (
+          mapProjection(good["Coordinates"]["to_country"])[1] -
+          line.label.verticalOffset +
+          "px"
+        );
       }
-      return mapProjection(good["Coordinates"]["from_country"])[1] + "px";
+      return (
+        mapProjection(good["Coordinates"]["from_country"])[1] -
+        line.label.verticalOffset +
+        "px"
+      );
     })
     .style("left", function(good, i) {
       if (i == 0) {
-        return mapProjection(good["Coordinates"]["to_country"])[0] + "px";
+        return (
+          mapProjection(good["Coordinates"]["to_country"])[0] -
+          (good["to_country"].length / 2) * line.label.letterWidth +
+          "px"
+        );
       }
-      return mapProjection(good["Coordinates"]["from_country"])[0] + "px";
+      return (
+        mapProjection(good["Coordinates"]["from_country"])[0] -
+        (good["from_country"].length / 2) * line.label.letterWidth +
+        "px"
+      );
     })
     .html(function(good, i) {
       if (i == 0) {
@@ -253,7 +212,41 @@ function createCountryID(country) {
     .replace(/,/, "");
 }
 
-// remove a country from the display and display list
+// add a country to the display and toolbar if it is not already on the display
+function addCountry() {
+  if (!activeCountryIDs.has($("#add-country-select").val())) {
+    // visualize the trade lines
+    addedCountry = $("#add-country-select").val();
+    activeCountryIDs.set(addedCountry, createCountryID(addedCountry));
+    visualizeTrade();
+
+    // add the country to the toolbar view list
+    d3.select("#current-view-list")
+      .append("li")
+      .attr("width", "100%")
+      .attr("id", activeCountryIDs.get(addedCountry) + "-list-element")
+      .append("h5")
+      .attr("class", "d-flex")
+      .html(
+        "<i class='fas fa-square mr-2' style='color:" +
+          line.colors[colorIndex] +
+          "'></i>" +
+          addedCountry +
+          "<button type='button' class='remove-country-button' value='" +
+          addedCountry +
+          "'><i class='fas fa-minus-circle'></i></button>"
+      );
+
+    $(".remove-country-button:last").on("click", function() {
+      console.log("hello!");
+      removeCountry($(this).val());
+    });
+
+    console.log("Added " + addedCountry + " to display!");
+  }
+}
+
+// remove a country from the display and toolbar
 function removeCountry(countryToDelete) {
   var countryToDeleteID = activeCountryIDs.get(countryToDelete);
 
